dafd25a369c78c8d21e9cd6faf3bb9f6e091c01c
Magnus Jonsson <magnus@gnollsmustdie.com>
2011-04-01 13:23:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paranoia::Application.config.secret_token = '5834c879a00d7e0974e433acefdae65e672e0bef173a0ff5d8cdc76b563deb21b597b73214d4f413eddb24866165c0b9d54877e7699b7898897c8363778cae85'
