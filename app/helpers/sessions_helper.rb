97dc6c1336fadc1d53f879d95a50499ea87833e4
Magnus Jonsson <magnus@gnollsmustdie.com>
2011-04-02 08:45:45 +0200

    Silly readme. Tricks are for kids.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,18 @@ module SessionsHelper
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
     self.current_user = user
   end
+
+  def sign_out
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
   
   def current_user=(user)
     @current_user = user
+  end
+  
+  def current_user
+    @current_user ||= user_from_remember_token
   end
   
   def signed_in?
