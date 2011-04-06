44c9c8c63ed8ebe8fb0e9c02cfb87bb366e51007
MostlyMagnus <magnus@gnollsmustdie.com>
2011-04-06 08:48:35 -0700

    Edited app/models/updater.rb via GitHub

--- app/models/updater.rb
+++ app/models/updater.rb
@@ -1,9 +1,7 @@
 class Updater
   
-  def update
-    Gamestate.find_by_id(current_user.session_id).update_when
- 
-    
+  def update    
+
     if Gamestate.find_by_id(current_user.session_id).update_when < Time.now
       crunch
     end
