e65a033e9a87530befc1b30a472e2a0253d88c8c
Magnus Jonsson <magnus@gnollsmustdie.com>
2011-04-06 21:16:06 +0200

    Updates to updater

--- app/models/updater.rb
+++ app/models/updater.rb
@@ -1,6 +1,8 @@
 class Updater
   def update
     # We do one get from the gamestate table to avoid too many queries
+    
+    # We need to get the current user in here some how
     gamestate = Gamestate.find_by_id(current_user.session_id)
  
     if gamestate.update_when < Time.now
@@ -12,7 +14,23 @@ class Updater
   private  
     def crunch
       
-      # for each turn that needs to be done      
+      #figure out how many turns we need to do
+
+      # this is very ugly - figure out a better way
+      @count = 0
+      
+      @gamestateTemp = gamestate
+      @timeTemp = Time.now
+      
+      while gamestateTemp.update_when < timeTemp.now do
+        @count = count + 1
+        
+        @gamestateTemp.update_when.advance(:hours => 2*(gamestate.timescale*1))  
+      end
+      # end of ugly
+      
+      
+      # for each turn that needs to be done          
       gamestate.update_when.advance(:hours => 2*(gamestate.timescale*1))  
     end
 end
