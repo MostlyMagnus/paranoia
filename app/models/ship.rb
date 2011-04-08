7fa44a0df381bc5728b9b943d4a57cecd698d1e3
Magnus Jonsson <magnus@gnollsmustdie.com>
2011-04-08 06:22:31 +0200

    Generated ship model, annotated. Cleaned up after removal of Updater class.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,1 +1,16 @@
-#ship class
+# == Schema Information
+# Schema version: 20110408041711
+#
+# Table name: ships
+#
+#  id          :integer         not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  image       :string(255)
+#  layout      :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+class Ship < ActiveRecord::Base
+end
