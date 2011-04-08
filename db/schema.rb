3690e8ca7ca6d4fa5c496b1b461969a52706bd6d
Magnus Jonsson <magnus@gnollsmustdie.com>
2011-04-08 22:56:55 +0200

    First incredibly ugly pass on actions and action queues

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110408171541) do
+ActiveRecord::Schema.define(:version => 20110408204913) do
 
   create_table "actions", :force => true do |t|
     t.integer  "pawn_id"
     t.integer  "queue_number"
-    t.integer  "type"
+    t.integer  "action_type"
     t.string   "params"
     t.datetime "created_at"
     t.datetime "updated_at"
