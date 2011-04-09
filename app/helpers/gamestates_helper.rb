6d4a93b2506c3950b64c84eb8ce375995974fb89
Magnus Jonsson <magnus@gnollsmustdie.com>
2011-04-09 13:08:27 +0200

    Another pass on actions. More gamestate-centric now.

--- app/helpers/gamestates_helper.rb
+++ app/helpers/gamestates_helper.rb
@@ -1,7 +1,7 @@
 module GamestatesHelper
   def updateGamestate
     gamestate = Gamestate.find_by_id(params[:id])
-      
+    
     if gamestate.update_when < Time.now
       crunch(gamestate)
     else
@@ -11,67 +11,7 @@ module GamestatesHelper
     
   def nextUpdate 
     Gamestate.find_by_id(params[:id]).update_when.localtime
-  end
-  
-  def aq
-    @gamestate = Gamestate.find_by_id(params[:id])
-    @pawns = Pawn.find_all_by_gamestate_id(@gamestate.id)    
-
-    action_queue = Array.new
-    
-    for i in 0..4 
-      @pawns.each do |p|
-        if !p.actions[i].nil?
-          
-          case p.actions[i].action_type
-            when ActionTypeDef::A_NIL
-              
-              action_queue.push(A_Nil.new(p.actions[i]))
-              
-            when ActionTypeDef::A_USE
-              
-              action_queue.push(A_Use.new(p.actions[i]))
-              
-            when ActionTypeDef::A_REPAIR
-              
-              action_queue.push(A_Repair.new(p.actions[i]))
-              
-            when ActionTypeDef::A_KILL
-              
-              action_queue.push(A_Kill.new(p.actions[i]))
-              
-          end
- 
-        end
-      end
-      
-      # Lets sort
-      action_queue.sort! { |a,b| a.priority <=> b.priority }
-      
-      # The array is now sorted on priority, with the LOWEST priority value first.
-      concat " Action Turn "
-      concat i
-      concat " [[[ "
-      for j in 0..action_queue.size-1
-        concat " "
-        concat action_queue[j]
-        concat " at prio "
-        concat action_queue[j].priority
-        concat " "
-      end
-      concat " ]]] "
- 
-      # Here we have our action_queue complete
-      # We should empty it.
-      action_queue.clear
-    end
-    
-    #for i in 0..action_queue.size-1
-    #  concat action_queue[i].kind_of? A_Nil
-    #  concat action_queue[i].priority
-    #end
-    
-  end
+  end 
     
   def currentTurn
     gamestate = Gamestate.find_by_id(params[:id])
@@ -83,9 +23,7 @@ module GamestatesHelper
     # Since users won't be able to queue up more than one turn worth of actions, and several
     # turns will only happen when NO ONE has activated the gamestate for a given time, we can
     # do this outside of the turn loop, and then clear the user queues.
-    
-    Pawn.find_all_by_gamestate_id(gamestate.id).each do |p|      
-    end
+    gamestate.buildExecuteAndClearActions
 
     # Now let's do some idle logic for the correct amount of turns
     @updatesRequired = ((Time.now - gamestate.update_when)/(3600 * gamestate.timescale)).floor
--- app/models/gamestate.rb
+++ app/models/gamestate.rb
@@ -15,5 +15,84 @@
 #
 
 class Gamestate < ActiveRecord::Base
+  def buildExecuteAndClearActions 
+    # This 
+    @pawns = Pawn.find_all_by_gamestate_id(self.id) 
+    @action_queue = Array.new
+    
+    for tick in 1..5
+      buildActionQueue(tick)
+      sortActionQueue
+      
+      executeActionQueue
+      
+      clearActionQueue
+    end
+    
+  end
+  
+  private
+  
+  def clearActionQueue
+    @action_queue.clear
+  end
+  
+  def buildActionQueue(action_tick)
+   
+    @pawns.each do |p|
+      if !p.actions[action_tick].nil?
+        case p.actions[action_tick].action_type
+          when ActionTypeDef::A_NIL
+            
+            @action_queue.push(A_Nil.new(p.actions[action_tick]))
+            
+          when ActionTypeDef::A_USE
+            
+           @action_queue.push(A_Use.new(p.actions[action_tick]))
+            
+          when ActionTypeDef::A_REPAIR
+            
+            @action_queue.push(A_Repair.new(p.actions[action_tick]))
+            
+          when ActionTypeDef::A_KILL
+            
+            @action_queue.push(A_Kill.new(p.actions[action_tick]))
+            
+        end
+      end
+    end  
+  end
+  
+  def sortActionQueue 
+    @action_queue.sort! { |a,b| a.priority <=> b.priority }
+  end
+  
+  def executeActionQueue
+    for i in 0..@action_queue.size-1
+      executeAction(@action_queue[i])
+    end
+  end
+  
+  def executeAction(action)
+    if      (action.kind_of? A_Nil)     then  executeA_Nil(action)
+    elsif   (action.kind_of? A_Use)     then  executeA_Use(action)
+    elsif   (action.kind_of? A_Repair)  then  executeA_Repair(action)
+    elsif   (action.kind_of? A_Kill)    then  executeA_Kill(action)
+    end
+  end
+   
+  # Question is, do we parse the params here, or when we push things into 
+  # @action_queue?
+  def executeA_Nil(action)
+  end
+  
+  def executeA_Use(action)
+  end
+  
+  def executeA_Repair(action)
+  end
+  
+  def executeA_Kill(action)
+  end
   
 end
