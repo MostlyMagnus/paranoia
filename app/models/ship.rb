b09e7e3fb061c959f47918852d3e7d3ffd670b8f
MostlyMagnus <magnus@gnollsmustdie.com>
2011-04-11 04:22:15 -0700

    Quick update to splitShip loop. Maybe it works better.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -27,19 +27,18 @@ class Ship < ActiveRecord::Base
     # 0,0;x00x;g1;n1$1,0;x0x0;c1;$2,0;x0x0;c1;$3,0;xx00;c1$0,1;0x0x;c1;$3,1;0x0x;c1;$0,2;0xrx;e;n0,n2$3,2;1xrx;b;n3$0,3;r0xx;e;n0,n2$1,3;x0x0;c1;$2,3;x0x0;c1;$3,3;rxx0;b;n3$
     splitShip = layout.split("$")
     
-    for i in 0..splitShip.size()-1
-        splitRoom = splitShip[i].split(";")
-        
-        # First we get the position
-        pos = S_Position.new(Integer(splitRoom[0].split(",")[0]), Integer(splitRoom[0].split(",")[1]))
- 
-        # Then we get the access codes
-        
-        access = S_Access.new(splitRoom[1][0], splitRoom[1][1], splitRoom[1][2], splitRoom[1][3])
+    splitShip.each do |room|
+      splitRoom = room.split(";")
+      
+      # First we get the position
+      pos = S_Position.new(Integer(splitRoom[0].split(",")[0]), Integer(splitRoom[0].split(",")[1]))
 
-        # Lets put it in our array        
-        @rooms[pos.hash] = Room.new(pos, access, splitRoom[2], splitRoom[3])
+      # Then we get the access codes
+      
+      access = S_Access.new(splitRoom[1][0], splitRoom[1][1], splitRoom[1][2], splitRoom[1][3])
 
+      # Lets put it in our array        
+      @rooms[pos.hash] = Room.new(pos, access, splitRoom[2], splitRoom[3])
     end
         
   end   
